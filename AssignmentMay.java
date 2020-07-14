8c6d92402ade7e172c8be50546572ef53718beff
Theofulis <philusmashego@gmail.com>
2020-07-14 08:49:53 +0200

    Update AssignmentMay.java
    changed all lines

--- AssignmentMay.java
+++ AssignmentMay.java
@@ -1,7 +1,13 @@
 Class AssignmentMay {
-    public static void main(String[] Args){
-        system.out.println("Hello World");
-        system.out.println("Hello World2");
+    public static void main(String[] args){
+        
+        int num1 = 2;
+        int num2= 3;
+        
+        
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println(num1+num2);
         
     }
 }
