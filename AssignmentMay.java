032f0068332c9a0c1ec3d2d79f1f14a66ac9ac54
Theofulis <philusmashego@gmail.com>
2020-07-14 08:54:18 +0200

    Update AssignmentMay.java

--- AssignmentMay.java
+++ AssignmentMay.java
@@ -6,7 +6,7 @@ Class AssignmentMay {
         
         
         System.out.println("hello world");
-        System.out.println("hello world");
+       
         System.out.println(num1+num2);
         
     }
